845721837fcadbc282fdd74359052043a1004433
Sara Hender <hender.sara@gmail.com>
2017-02-12 15:01:09 -0800

    Plus the collection view.

---
+++ Animal ABCs/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  Animal ABCs
+//
+//  Created by Sara Hender on 2/12/17.
+//  Copyright © 2017 Sara Hender. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    
+    
+}
